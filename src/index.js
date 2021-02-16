50bad2b5a11f425c527a72d68c710c1e542c152c
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-16 18:07:53 +0300

    init: add my code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    str = str.split('');
+    for (let b = 0; b < bracketsConfig.length; b++) {
+        let x0 = bracketsConfig[b][0];
+        let x1 = bracketsConfig[b][1];
+
+         for (let i=0; i < str.length; i++) {
+             let i2 = i+1;
+             if (str[i] == x0 && str[i2] == x1) {
+                 str.splice(i, 2);
+                 i = -1; b = -1;
+             }
+         }
+    }
+
+     if (str.length == 0) {
+         return console.log(true);
+     } else return console.log(false)
+
 }
