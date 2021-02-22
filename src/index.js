1998daff15a2ab703c154c036fdcb2b9d5937cee
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-22 14:57:33 +0300

    refactor: short code

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ module.exports = function check(str, bracketsConfig) {
     for (let b = 0; b < bracketsConfig.length; b++) {
         let x0 = bracketsConfig[b][0];
         let x1 = bracketsConfig[b][1];
-
-        for (let i=0; i < str.length; i++) {            
-            if (str[i] == x0 && str[i+1] == x1) {
-                str.splice(i, 2);
-                i = -1; b = -1;
+            for (let i=0; i < str.length; i++) {
+                if (str[i] == x0 && str[i+1] == x1) {
+                    str.splice(i, 2);
+                    i = -1; b = -1;
+                }
             }
-        }
     }
-
-    return !(str.length); 
+    return !(str.length);
 }
